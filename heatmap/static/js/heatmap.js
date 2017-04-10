3edaf0d8cfc6be19d3b65f035a0989069d8d5ff0
pvlbzn <pvlbzn@gmail.com>
2017-04-10 19:57:24 +0200

    Render separate event, enum like settings

--- heatmap/static/js/heatmap.js
+++ heatmap/static/js/heatmap.js
@@ -4,6 +4,11 @@
  *
  * @param {string} apiPath API path
  * @param {string} containerId container ID that the map will be attached to
+ * @param {string} kind what kind of data will be rendered. Possible states
+ *                      are wrapped into DataKind object.
+ *                      - DataKind.RENDER_LEADS  to render leads
+ *                      - DataKind.RENDER_EVENTS to render events
+ *                      - DataKind.RENDER_EMPTY  to render neither leads nor events
  * @param {{lat: float, lng: float}} initialRegion initial coordinates that the map is centered on
  * @param {bool} isPolitical renders a political map if this is set to true and a geographical map otherwise
  * @param {bool} showHeatmap renders the heatmap if set to true
@@ -11,6 +16,7 @@
  */
 function generateMap(apiPath,
                    containerId,
+                   kind=DataKind.RENDER_EMPTY,
                    initialRegion={lat: 39.50, lng: -98.35},
                    isPolitical=true,
                    showHeatmap=true,
@@ -24,11 +30,11 @@ function generateMap(apiPath,
      * @class Marker
      */
     class Marker {
-        constructor(address, title, id, zip) {
+        constructor(address, title, id, loc) {
             this.address = address;
             this.title = title;
             this.id = id;
-            this.zip = zip;
+            this.loc = loc;
         }
     }
 
@@ -263,7 +269,7 @@ function generateMap(apiPath,
         data.events.forEach((event) => {
             let pureLocations = dataSentinel(event.locations);
 
-            pureLocations.forEach((location) => {
+            pureLocations.forEach(location => {
                 markers.push(new Marker(
                     event.address,
                     event.title,
@@ -276,15 +282,16 @@ function generateMap(apiPath,
     }
 
     function renderMarkers(markers) {
-        markers.forEach((marker) => {
+        markers.forEach(marker => {
             let m = new google.maps.Marker({
                 // Performance issue on a big dataset
                 // animation: google.maps.Animation.DROP,
                 map: map,
-                position: marker.zip,
+                position: marker.loc,
                 title: marker.title
             });
 
+            // Block uses `this` pointer
             google.maps.event.addListener(m, 'click', function () {
                 let info = new google.maps.InfoWindow();
                 info.setContent(
@@ -299,9 +306,9 @@ function generateMap(apiPath,
     function renderHeatmap(markers) {
         let coord = [];
 
-        markers.forEach((marker) => {
+        markers.forEach(marker => {
             coord.push(
-                new google.maps.LatLng(marker.zip)
+                new google.maps.LatLng(marker.loc)
             );
         });
 
@@ -314,50 +321,131 @@ function generateMap(apiPath,
     }
 
     /**
+     * Render Event marker on a map.
+     * 
+     * Function works a little different from renderHeatmap or renderMarkers.
+     * Those two take a list of Marker objects, while renderEvent takes
+     * an address and a title as arguments and renders them directly avoiding
+     * object serialization.
+     * 
+     * @param {string} addr event address in a human readable string
+     * @param {string} title event title
+    
+     */
+    function renderEvent(addr, title) {
+        geocoder.geocode({'address': addr}, (res, status) => {
+            if (status == 'OK') {
+                let m = new google.maps.Marker({
+                    map: map,
+                    position: res[0].geometry.location,
+                    titile: title
+                });
+
+                // Block uses `this` pointer
+                google.maps.event.addListener(m, 'click', function () {
+                    let info = new google.maps.InfoWindow();
+                    info.setContent(
+                        '<b>Address:</b> ' + addr + '<br>' +
+                        '<b>Title:</b> ' + title + '<br>');
+                    info.open(map, this);
+                });
+            } else {
+                console.error('Geocode failure');
+            }
+        });
+    }
+
+    /**
      * Data sentinel ensures data consistency by filtering out points that are not located in the US.
      *
      * Source: http://answers.google.com/answers/threadview?id=149284
      */
     function dataSentinel(data) {
         let north_lat = 49.3457868; // top
-        let west_lng = -124.7844079; // left
-        let east_lng = -66.9513812; // right
+        let west_lng  = -124.7844079; // left
+        let east_lng  = -66.9513812; // right
         let south_lat = 24.7433195; // bottom
 
         let clean = data.filter(point =>
             south_lat <= point.lat &&
             point.lat <= north_lat &&
-            west_lng <= point.lng &&
+            west_lng  <= point.lng &&
             point.lng <= east_lng);
 
         return clean;
     }
 
-    function renderUI() {
-        if (showHeatmap && showMarkers) {
-            fetch(apiPath, data => {
-                let parsedData = JSON.parse(data);
-                let markers = collectMarkers(parsedData);
-                renderHeatmap(markers);
-                renderMarkers(markers);
-            });
+    function renderUI(kind) {
+        switch (kind) {
+            case 'RENDER_LEADS':
+                if (showHeatmap && showMarkers) {
+                    fetch(apiPath, data => {
+                        let parsedData = JSON.parse(data);
+                        let markers = collectMarkers(parsedData);
+                        renderHeatmap(markers);
+                        renderMarkers(markers);
+                    });
 
-        } else if (showHeatmap) {
-            fetch(apiPath, data => {
-                let parsedData = JSON.parse(data);
-                let markers = collectMarkers(parsedData);
-                renderHeatmap(markers);
-            });
-            
-        } else if (showMarkers) {
-            fetch(apiPath, data => {
-                let parsedData = JSON.parse(data);
-                let markers = collectMarkers(parsedData);
-                renderMarkers(markers);
-            });
+                } else if (showHeatmap) {
+                    fetch(apiPath, data => {
+                        let parsedData = JSON.parse(data);
+                        let markers = collectMarkers(parsedData);
+                        renderHeatmap(markers);
+                    });
+                    
+                } else if (showMarkers) {
+                    fetch(apiPath, data => {
+                        let parsedData = JSON.parse(data);
+                        let markers = collectMarkers(parsedData);
+                        renderMarkers(markers);
+                    });
+                }
+            break;
+
+            case 'RENDER_EVENTS':
+                fetch(apiPath, data => {
+                    let parsedData = JSON.parse(data);
+                    
+                    parsedData.events.forEach(event => {
+                        renderEvent(event.address, event.title);
+                    });
+
+                    if (showHeatmap) {
+                        let markers = collectMarkers(parsedData);
+                        renderHeatmap(markers);
+                    }
+
+                    if (showMarkers) {
+                        console.error('Kind.RENDER_EVENTS and showMarkers=true at the same time, it shouldnt be so');
+                    }
+                })
+            break;
+
+            case '':
+                // Do nothing, just check function call consistency
+            break;
+
+            default:
+                console.error('kind paramenter must be provided to the generateMap function');
         }
     }
 
     const map = createUI();
-    renderUI();
+    const geocoder = new google.maps.Geocoder();
+    renderUI(kind);
+}
+
+const DataKind = {
+    'RENDER_LEADS': 'RENDER_LEADS',
+    'RENDER_EVENTS': 'RENDER_EVENTS',
+    'RENDER_EMPTY': ''
+}
+
+const MapKind = {
+    'POLITICAL': true,
+    'CLASSIC': false,
+    'HEATMAP': true,
+    'NO_HEATMAP': false,
+    'MARKERS': true,
+    'NO_MARKERS': false
 }
